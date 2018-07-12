bf898b217b00e2fe85c04a1b979b95072b6d3f28
freeongoo <freeongoo@gmail.com>
2018-07-12 23:36:33 +0300

    init

---
+++ src/main/java/com/obfuscation/App.java
@@ -0,0 +1,13 @@
+package com.obfuscation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
